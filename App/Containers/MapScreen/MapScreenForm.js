e4892accd1c8eb523331666ee7dfe3f6ecff9566
syedhassanabbas12 <hassanabbassayed@gmail.com>
2019-04-04 23:17:16 +0500

    Configured Markers with onPress callout + Navigation to SchoolDetail_Screen

--- App/Containers/MapScreen/MapScreenForm.js
+++ App/Containers/MapScreen/MapScreenForm.js
@@ -1,22 +1,66 @@
 import React, { Component } from 'react'
 import { View} from 'react-native'
 import { Item as FormItem, Text, Button, Input } from 'native-base'
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { Marker, PROVIDER_GOOGLE, Callout } from 'react-native-maps'
 import styles from './styles'
 
 
 class MapScreenForm extends Component{
   constructor(props) {
     super(props);
-  
   }	
 
+
   state = {
+    markers: [
+      {
+        coordinate: {
+          latitude: 31.5844056,
+          longitude: 74.3552687,
+        },
+        title: "GHSS COMPRESHENSIVE GHORAY SHAH",
+        description: "SHALIMAR	SECONDARY-WING	H.Sec.",
+        // image: Images[0],
+      },
+      {
+        coordinate: {
+          latitude: 31.5583924,
+          longitude: 74.3511501,
+        },
+        title: "GGHSS SHEIKH SARDAR MUHAMMAD GARHI SHAHU",
+        description: "SHALIMAR	SECONDARY-WING	H.Sec.",
+        // image: Images[1],
+      },
+      {
+        coordinate: {
+          latitude: 31.5420496,
+          longitude: 74.3057628,
+        },
+        title: "GGHSS SAMANABAD",
+        description: "CITY	SECONDARY-WING	H.Sec.",
+        // image: Images[2],
+      },
+      {
+        coordinate: {
+          latitude: 31.4798024,
+          longitude: 74.5116445,
+        },
+        title: "GGHSS BARKI",
+        description: "CANTT	SECONDARY-WING	H.Sec.",
+        // image: Images[3],
+      },
+    ],
+    region: {
+      latitude: 31.5376684,
+      longitude: 74.2776854,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+    },
     mapRegion: null,
     lastLat: null,
     lastLong: null,
-  }
-  
+    school:''
+  };
 
   componentDidMount() {
     this.watchID = navigator.geolocation.watchPosition((position) => {
@@ -40,40 +84,38 @@ class MapScreenForm extends Component{
     });
   }
 
-  // onMapPress(e) {
-  //   let region = {
-  //     latitude:       e.nativeEvent.coordinate.latitude,
-  //     longitude:      e.nativeEvent.coordinate.longitude,
-  //     latitudeDelta:  0.00922*1.5,
-  //     longitudeDelta: 0.00421*1.5
-  //   }
-  //   this.onRegionChange(region, region.latitude, region.longitude);
-  // }
+  gotoSchoolDetail = () => {
+    const { navigation } = this.props
+      navigation.navigate("SchoolDetail",{id: this.state.school})
+  }
+
   
   render(){
     return(
-    // <View style={styles.container}>
-    //     <MapView
-    //         provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-    //         style={styles.map}
-    //         region={{
-    //         latitude: 37.78825,
-    //         longitude: -122.4324,
-    //         latitudeDelta: 0.015,
-    //         longitudeDelta: 0.0121,
-    //       }}
-    //     >
-    //     </MapView>
-    //   </View>
       <View style={styles.container}>
         <MapView
             provider={PROVIDER_GOOGLE} // remove if not using Google Maps
             style={styles.map}
             region={this.state.mapRegion}
             // onRegionChange={this.onRegionChange.bind(this)}
-            // showsMyLocationButton={true}
             showsUserLocation={true}
+            showsMyLocationButton={true}
         >
+
+            {this.state.markers.map((marker, index) => {
+              return (
+                <MapView.Marker
+                key={index}
+                coordinate={marker.coordinate}
+                title={marker.title}
+                description={marker.description}
+                >
+                  <Callout onPress={()=>this.gotoSchoolDetail()}>
+                  </Callout>
+                </MapView.Marker>  
+              );
+            })}
+
         </MapView>
       </View>
     )

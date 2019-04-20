0697322dad224ecb9bdad287d8c2edd302d487a2
SajjadMustafa <sajjad.mustafa@ucp.edu.pk>
2019-04-20 11:14:32 +0500

    css working

--- App/Containers/ManualSchoolSelect/index.js
+++ App/Containers/ManualSchoolSelect/index.js
@@ -1,116 +1,124 @@
 import React, { Component } from 'react'
-import { View,Picker,StyleSheet} from 'react-native'
-import { Item as FormItem, Text, Button, Input ,Header,Body,Title,Left,Right} from 'native-base'
+import { View, Picker, StyleSheet } from 'react-native'
+import { Item as FormItem, Text, Button, Input, Header, Body, Title, Left, Right } from 'native-base'
 import { connect } from 'react-redux'
 import Actions from '../../Redux/Actions'
 import { createStructuredSelector } from 'reselect'
 import { get } from 'lodash'
 
-class ManualSchoolSelect extends Component{
+class ManualSchoolSelect extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {district: '',tehsil:'',school:'',uniqueDistrict:[]
-										,uniqueTehsil:[],uniqueschool:[]};
+		this.state = {
+			district: '', tehsil: '', school: '', uniqueDistrict: []
+			, uniqueTehsil: [], uniqueschool: []
+		};
 
 		this.props.allSchoolsData()
-		.then(() => {
-			let uniqueDist = [...new Set(get(this.props, 'allSchools').map(school => school.district))];
-			this.setState({uniqueDistrict: uniqueDist})
-			
-			let uniqueTeh = [...new Set(get(this.props, 'allSchools').map(school => school.tehsil))];
-			this.setState({uniqueTehsil: uniqueTeh})
+			.then(() => {
+				let uniqueDist = [...new Set(get(this.props, 'allSchools').map(school => school.district))];
+				this.setState({ uniqueDistrict: uniqueDist })
 
-			this.setState({uniqueschool: this.props.allSchools})
-			console.log('this.state.uniqueschool',this.state.uniqueschool)
+				let uniqueTeh = [...new Set(get(this.props, 'allSchools').map(school => school.tehsil))];
+				this.setState({ uniqueTehsil: uniqueTeh })
 
-			
-		})
+				this.setState({ uniqueschool: this.props.allSchools })
+				console.log('this.state.uniqueschool', this.state.uniqueschool)
+
+
+			})
 	}
 
 	updateDistrict = (district) => {
-		this.setState({district: district})
-		this.props.uniqueSchoolData({district, tehsil:''})
-		.then(() => {
-			let uniqueTeh = [...new Set(get(this.props, 'uniqueData').map(school => school.tehsil))];
-			 this.setState({ uniqueTehsil:  uniqueTeh})
-		})
+		this.setState({ district: district })
+		this.props.uniqueSchoolData({ district, tehsil: '' })
+			.then(() => {
+				let uniqueTeh = [...new Set(get(this.props, 'uniqueData').map(school => school.tehsil))];
+				this.setState({ uniqueTehsil: uniqueTeh })
+			})
 	}
 
 	updateTehsil = (tehsil) => {
-		this.setState({ tehsil:tehsil})
-		this.props.uniqueSchoolData({district:'',tehsil})
-		.then(() => {
-			this.setState({ uniqueschool:  this.props.uniqueData})
-		})
+		this.setState({ tehsil: tehsil })
+		this.props.uniqueSchoolData({ district: '', tehsil })
+			.then(() => {
+				this.setState({ uniqueschool: this.props.uniqueData })
+			})
 	}
 
 	updateSchool = (school) => {
-		this.setState({ school:school })
+		this.setState({ school: school })
 	}
 
 
 	gotoSchoolDetail = () => {
-    const { navigation } = this.props
-      navigation.navigate("SchoolDetail",{id: this.state.school})
-  }
+		const { navigation } = this.props
+		navigation.navigate("SchoolDetail", { id: this.state.school })
+	}
 
-	render(){
+	render() {
 
 
-		return(
-			<View style = {{ 
-					flex:1,
-					flexDirection:'column',
-					justifyContent:'space-between',
-					 }}>
+		return (
+			<View style={{
+				flex: 1,
+				flexDirection: 'column',
+				justifyContent: 'space-between',
+			}}>
 
-          <Header>
-          
-            {/* <Left>
+				<Header>
+
+					{/* <Left>
             <Button>
                 <Text>Notification</Text>
               </Button>
             </Left>
              */}
-            <Body>
-              <Title>Manual Select</Title>
-            </Body>
+					<Body>
+						<Title>Manual Select</Title>
+					</Body>
 
-            {/* <Right>
+					{/* <Right>
               <Button transparent dark>
               <Image source={Images.Notification} style={{ width: 35,height:35,}} />                            
               </Button>
             </Right> */}
 
-          </Header>
-   	
-			
-
-			<Picker selectedValue = {this.state.district} 
-														 onValueChange = {(itemValue, itemIndex)=>this.updateDistrict(itemValue)}>
-					{this.state.uniqueDistrict && this.state.uniqueDistrict.map((district) =>
-				<Picker.Item label = {district} value = {district} />)}
-			</Picker>
-
-		
-			<Picker selectedValue = {this.state.tehsil}
-													onValueChange = {(itemValue, itemIndex) => this.updateTehsil(itemValue)}>
-				{this.state.uniqueTehsil && this.state.uniqueTehsil.map((tehsil) =>
-					<Picker.Item label = {tehsil} value = {tehsil} />)
-				}
-			</Picker>
+				</Header>
 
 
-			<Picker selectedValue = {this.state.school} 
-														onValueChange = {(itemValue,itemIndex) => this.updateSchool(itemValue)}>
-				{this.state.uniqueschool && this.state.uniqueschool.map((school) =>
-				<Picker.Item label = {school.school_name} value = {school} />)}
-			</Picker>
+				<Text>Select District</Text>
+				<View  style={styles.container}>
+					<Picker selectedValue={this.state.district}
+						onValueChange={(itemValue, itemIndex) => this.updateDistrict(itemValue)}>
+						{this.state.uniqueDistrict && this.state.uniqueDistrict.map((district) =>
+							<Picker.Item label={district} value={district} />)}
+					</Picker>
+				</View>
+
+				<Text>Select Tehsil</Text>
+				<View  style={styles.container}>
+				<Picker selectedValue={this.state.tehsil}
+					onValueChange={(itemValue, itemIndex) => this.updateTehsil(itemValue)}>
+					{this.state.uniqueTehsil && this.state.uniqueTehsil.map((tehsil) =>
+						<Picker.Item label={tehsil} value={tehsil} />)
+					}
+				</Picker>
+				</View>
+
+				<Text>Select School</Text>
+				<View  style={styles.container}>
+				<Picker selectedValue={this.state.school}
+					onValueChange={(itemValue, itemIndex) => this.updateSchool(itemValue)}>
+					{this.state.uniqueschool && this.state.uniqueschool.map((school) =>
+						<Picker.Item label={school.school_name} value={school} />)}
+				</Picker>
+				</View>
 
 
-			<Button style={{alignSelf: 'center',width: '50%'}}    
-								onPress={this.gotoSchoolDetail}> 
-					<Text style={{width: '100%',fontWeight: "800",textAlign: "center"}}>
+				<Button style={{ alignSelf: 'center', width: '50%' }}
+					onPress={this.gotoSchoolDetail}>
+					<Text style={{ width: '100%', fontWeight: "800", textAlign: "center" }}>
 						Ok
 					</Text>
 				</Button>
@@ -125,7 +133,7 @@ class ManualSchoolSelect extends Component{
 }
 
 
-const mapStateToProps = createStructuredSelector ({
+const mapStateToProps = createStructuredSelector({
 	allSchools: (state) => get(state, 'school.allSchoolsData'),
 	uniqueData: (state) => get(state, 'school.uniqueSchoolsData')
 })
@@ -140,11 +148,15 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManualSchoolSelect)
-	
-	const styles = StyleSheet.create({
-		text: {
-			 fontSize: 30,
-			 alignSelf: 'center',
-			 color: 'red'
-		}
- })
+
+const styles = StyleSheet.create({
+	text: {
+		fontSize: 30,
+		alignSelf: 'center',
+		color: 'red'
+	},
+	container: {
+    borderRadius: 10,
+    borderWidth: 1,
+  },
+})
